9d534dfa5822a90f02434217863b2fd44954469c
Ben Wiklund <ben@daisyowl.com>
2020-08-02 13:53:31 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,16 @@ fn main() {
         }
     }
 
+    while !file.buffer().is_empty() {
+        read_chunk(&mut file);
+    }
+}
+
+fn read_chunk(file: &mut BufReader<File>) {
     // CHUNKS
     // Chunk length
     println!("Chunk:");
-    let chunk_len = read_bytes(&mut file, 4, String::from("Chunk length"));
+    let chunk_len = read_bytes(file, 4, String::from("Chunk length"));
     // is this the best way to convince rustc that this is 4 bytes???
     let mut len_bytes = [0u8; 4];
     len_bytes.copy_from_slice(&chunk_len[0..4]);
@@ -32,19 +38,23 @@ fn main() {
     println!("- len: {} bytes", len);
 
     // Chunk type
-    let chunk_type = read_bytes(&mut file, 4, String::from("Chunk type"));
+    let chunk_type = read_bytes(file, 4, String::from("Chunk type"));
     println!("- type: {}", std::str::from_utf8(&chunk_type).unwrap());
 
     // Chunk data
-    let chunk_data = read_bytes(&mut file, len as u64, String::from("Chunk data"));
-    println!("- data: <...> (read {} bytes successfully)", chunk_data.len());
+    let chunk_data = read_bytes(file, len as u64, String::from("Chunk data"));
+    println!(
+        "- data: <...> (read {} bytes successfully)",
+        chunk_data.len()
+    );
 
     // Chunk CRC
-    let chunk_crc = read_bytes(&mut file, 4, String::from("Chunk CRC"));
+    let chunk_crc = read_bytes(file, 4, String::from("Chunk CRC"));
     let mut crc_bytes = [0u8; 4];
     crc_bytes.copy_from_slice(&chunk_crc[0..4]);
     let crc = u32::from_be_bytes(crc_bytes);
     println!("- crc: {}", crc); // TODO actually check this
+    println!();
 }
 
 fn read_bytes(buf_reader: &mut BufReader<File>, len: u64, error_msg: String) -> Vec<u8> {
